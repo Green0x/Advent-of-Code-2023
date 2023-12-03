3f2439a865bdeb24e2499e3bde1e9efd9b77df77
Bill <jamiepwheeler@gmail.com>
2023-12-03 17:16:59 +0000

    Change incorrect comments

--- Day 1/main.js
+++ Day 1/main.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 
 let input = fs.readFileSync('Day 1/input.txt', 'utf-8');
-let splitInput = input.split('\n'); // Remove all non numeric characters
+let splitInput = input.split('\n'); // Put each string into an array
 let finalNum = 0;
 
 for (let i = 0; i < splitInput.length; i++) {   
-    let elementToStr = splitInput[i].toString().replace(/\D/g, ''); // Change each element to a string for easy concatenation
+    let elementToStr = splitInput[i].toString().replace(/\D/g, ''); // Remove all non numeric characters
     let start = elementToStr.slice(0, 1); // Get first number
     let end = elementToStr.slice(-1); // Get last number
     splitInput[i] = start + end; // Concatenate both numbers and put back into array
